7d3f737dd34d94afb3305f5358350526f8970eb9
hailiang.gao <gaohl0501@gmail.com>
2019-09-11 13:58:52 +0800

    添加测试用例

---
+++ src/main/java/com/xushen/Test.java
@@ -0,0 +1,9 @@
+package com.xushen;
+
+public class Test {
+    public static void main(String[] args) {
+        String str="http://soa.beta.uledns.com/cishanProductService/product/getProductInfoByListingId";
+        System.out.println(str.replaceAll(pattern,"http://dfsread-service.http.dev.uledns.com/"));
+
+    }
+}
